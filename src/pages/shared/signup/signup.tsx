ae4841e70344cbee7cac82ad9de4319020cf010d
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-11-28 15:10:50 -0300

    Hotlflix

--- src/pages/shared/signup/signup.tsx
+++ src/pages/shared/signup/signup.tsx
@@ -1,61 +1,56 @@
-import React from 'react'
-import { useContext, useState } from 'react'
-import { z } from 'zod'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Link, useNavigate } from 'react-router-dom'
-import { Check, LoaderCircle, LockOpen, LucideCheck, ShieldCheck } from 'lucide-react'
-import { AuthContext } from '../../../auth/auth-context'
+import React, { useState } from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useNavigate } from 'react-router-dom';
+import { LockOpen, ShieldCheck, LoaderCircle } from 'lucide-react';
 import Form from '../../../components/form';
 import Button from '../../../components/button';
-import InputRoot from '../../../components/input-root'
-import Span from '../../../components/span'
-import Label from '../../../components/label'
-import InputText from '../../../components/input-text'
-import Section from '../../../components/section'
-import { signupService } from '../../../services/signup-service'
-import { useQuery } from '../../../utils/hooks/query-hooks'
-import LoadingContainer from '../../../base-components/loading'
-import { USER_ROUTES } from '../../../config/routes-config'
+import InputRoot from '../../../components/input-root';
+import Span from '../../../components/span';
+import Label from '../../../components/label';
+import InputText from '../../../components/input-text';
+import Section from '../../../components/section';
+import { signupService } from '../../../services/signup-service';
+import { useQuery } from '../../../utils/hooks/query-hooks';
+import { USER_ROUTES } from '../../../config/routes-config';
 
 interface SignupSchema {
-  name: string
-  username: string
-  lastName: string
-  email: string
-  phone: string
-  password: string
-  confirmPassword: string
+  name: string;
+  username: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+  confirmPassword: string;
 }
 
 function Signup() {
-  const [finished, setQuerys] = useQuery(true)
-  const [success, setSuccess] = useState(false)
+  const [finished, setQueries] = useQuery(true);
+  const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
 
   const signupSchema = z.object({
     name: z.string().nonempty("Required"),
     username: z.string().nonempty("Required"),
     lastName: z.string().nonempty("Required"),
-    email: z.string().nonempty("Required").email("E-Mail Inválido"),
+    email: z.string().nonempty("Required").email("Invalid E-Mail"),
     phone: z.string().nonempty("Required"),
     password: z.string().nonempty("Required"),
     confirmPassword: z.string().nonempty("Required")
-  })
-    .refine((data) => data.password === data.confirmPassword, {
-      path: ["confirmPassword"], // Local do erro
-      message: "As senhas não coincidem",
-    });
-  const { register, control, formState, handleSubmit } = useForm<SignupSchema>(
-    {
-      resolver: zodResolver(signupSchema),
-    }
-  );
+  }).refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
 
-  function handleSignUp(data: any) {
-    return signupService.signupPost(data).then(e => {
-      setSuccess(true)
-    })
+  const { register, formState, handleSubmit } = useForm<SignupSchema>({
+    resolver: zodResolver(signupSchema),
+  });
+
+  function handleSignUp(data: SignupSchema) {
+    return signupService.signupPost(data).then(() => {
+      setSuccess(true);
+    });
   }
 
   return (
@@ -64,89 +59,81 @@ function Signup() {
         <div className='flex items-center justify-center gap-6 flex-col'>
           <div className='flex gap-3 items-center mb-3'>
             <LockOpen className='w-9 h-9 text-main-violet-500' />
-            <h1 className='font-semibold text-3xl '>Safeguard</h1>
+            <h1 className='font-semibold text-3xl'>Safeguard</h1>
           </div>
-          {!success ?
+          {!success ? (
             <div className='gap-3 flex flex-col bg-main-black-800 shadow-sm rounded sm:px-12 px-6 py-12 sm:w-fit w-full'>
-              <Form onSubmit={handleSubmit((e) => setQuerys(() => handleSignUp(e)))}>
-                <h1 className='font-semibold text-2xl'>Criar conta</h1>
+              <Form onSubmit={handleSubmit((data) => setQueries(() => handleSignUp(data)))}>
+                <h1 className='font-semibold text-2xl'>Create Account</h1>
                 <InputRoot>
-                  <Label>Nome</Label>
-                  <InputText placeholder='Nome' {...register('name')} />
+                  <Label>Name</Label>
+                  <InputText placeholder='Name' {...register('name')} />
                   <Span variation='error'>{formState.errors.name?.message}</Span>
                 </InputRoot>
                 <InputRoot>
-                  <Label>Sobrenome</Label>
-                  <InputText placeholder='Sobrenome' {...register('lastName')} />
+                  <Label>Last Name</Label>
+                  <InputText placeholder='Last Name' {...register('lastName')} />
                   <Span variation='error'>{formState.errors.lastName?.message}</Span>
                 </InputRoot>
                 <InputRoot>
                   <Label>Username</Label>
-                  <InputText placeholder='Nome' {...register('username')} />
+                  <InputText placeholder='Username' {...register('username')} />
                   <Span variation='error'>{formState.errors.username?.message}</Span>
                 </InputRoot>
                 <InputRoot>
-                  <Label>E-Mail</Label>
-                  <InputText placeholder='E-Mail' {...register('email')} />
+                  <Label>Email</Label>
+                  <InputText placeholder='Email' {...register('email')} />
                   <Span variation='error'>{formState.errors.email?.message}</Span>
                 </InputRoot>
                 <InputRoot>
-                  <Label>Telefone</Label>
-                  <InputText placeholder='Telefone' {...register('phone')} />
+                  <Label>Phone</Label>
+                  <InputText placeholder='Phone' {...register('phone')} />
                   <Span variation='error'>{formState.errors.phone?.message}</Span>
                 </InputRoot>
                 <div>
                   <InputRoot>
-                    <Label>Senha</Label>
-                    <InputText placeholder='Senha' type="password" {...register('password')} />
+                    <Label>Password</Label>
+                    <InputText placeholder='Password' type="password" {...register('password')} />
                     <Span variation='error'>{formState.errors.password?.message}</Span>
                   </InputRoot>
                   <ul className="max-w-md mt-2 space-y-1 text-gray-500 list-disc list-inside text-sm">
-                    <li>
-                      Pelo menos uma letra maiúscula
-                    </li>
-                    <li>
-                      Um caractere especial
-                    </li>
-                    <li>
-                      Pelo menos um numero
-                    </li>
+                    <li>At least one uppercase letter</li>
+                    <li>One special character</li>
+                    <li>At least one number</li>
                   </ul>
                 </div>
                 <InputRoot>
-                  <Label>Confirmar senha</Label>
-                  <InputText placeholder='Confirmar senha' type="password" {...register('confirmPassword')} />
+                  <Label>Confirm Password</Label>
+                  <InputText placeholder='Confirm Password' type="password" {...register('confirmPassword')} />
                   <Span variation='error'>{formState.errors.confirmPassword?.message}</Span>
                 </InputRoot>
-                <Button loadingComponent={<LoaderCircle className={"rotating-div"} />} variation='default-full' loading={!finished}>
-                  Criar Conta
+                <Button loadingComponent={<LoaderCircle className="rotating-div" />} variation='default-full' loading={!finished}>
+                  Create Account
                 </Button>
               </Form>
-            </div> :
+            </div>
+          ) : (
             <div className='gap-6 flex flex-col center max-w-96 bg-main-black-800 shadow-sm rounded sm:px-12 px-6 py-12 sm:w-fit w-full'>
               <div className='flex items-center'>
-                <h1 className='font-semibold text-2xl'>Conta criada com sucesso</h1>
+                <h1 className='font-semibold text-2xl'>Account Successfully Created</h1>
                 <ShieldCheck className='w-16 h-16' strokeWidth={1.5} />
               </div>
-              <p >Agora você ja pode gerenciar suas contas.</p>
+              <p>You can now manage your accounts.</p>
               <Button variation='default-full' onClick={() => navigate(USER_ROUTES.LOGIN)}>
-                Voltar para tela de login
+                Back to Login
               </Button>
             </div>
-          }
-
+          )}
           <div className='flex gap-1 text-sm flex-wrap'>
-            <p>Já possui uma conta?</p>
-            <Link className='text-main-violet-500 font-semibold' to={"/login"}>Fazer login</Link>
+            <p>Already have an account?</p>
+            <Link className='text-main-violet-500 font-semibold' to={"/login"}>Log In</Link>
           </div>
         </div>
       </Section>
     </section>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
 
-export {
-  SignupSchema
-}
+export { SignupSchema };
