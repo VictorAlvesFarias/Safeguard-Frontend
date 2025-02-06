b2f60ac205229d0db9673ccd8ff4190774c08c7a
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2025-02-06 10:57:40 -0300

    Environment fix

--- src/environment.tsx
+++ src/environment.tsx
@@ -1,1 +1,10 @@
-export const env = "https://localhost:7240"
+let env = "https://localhost:7240"
+
+if (process.env.NODE_ENV === 'development') {
+    env = "http://localhost:7240"
+}
+else {
+    env = "http://localhost:8888"
+}
+
+export { env } 
