c84d4723b8620c2173731fb061884aa4d05bc327
Victor Alves Faria <victoralvesfarias2004@gmail.com>
2025-03-08 13:40:18 -0300

    Hotfix
    - Update environement.
    - Update vite config.
    - New icon.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"./",
+  base: "./",
   assetsInclude: ['**/*.svg'],
   build: {
-    chunkSizeWarningLimit: 1600
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('src/environment.ts')) { // Ajuste aqui para o caminho correto do seu arquivo
+            return 'environment'; // O nome do arquivo gerado
+          }
+        }
+      }
+    }
   }
 })
- 
